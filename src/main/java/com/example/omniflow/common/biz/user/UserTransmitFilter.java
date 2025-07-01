9c1283bba877a9a236ed1ec3caf851d6df3285fd
luo-jiu <z3201132097@gmail.com>
2025-07-01 12:47:42 +0800

    fix: 调整拦截器逻辑

--- src/main/java/com/example/omniflow/common/biz/user/UserTransmitFilter.java
+++ src/main/java/com/example/omniflow/common/biz/user/UserTransmitFilter.java
@@ -1,6 +1,5 @@
 package com.example.omniflow.common.biz.user;
 
-import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.example.omniflow.common.convention.exception.ClientException;
 import com.example.omniflow.common.convention.result.Results;
@@ -18,7 +17,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
@@ -26,37 +24,43 @@ public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
 
     private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/omniflow/v1/user",
             "/api/omniflow/v1/user/login",
             "/api/omniflow/v1/user/has-username"
     );
 
+    // TODO 未来加入本地缓存 防止多次请求redis
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String requestURI = httpServletRequest.getRequestURI();
-        if (!IGNORE_URI.contains(requestURI)) {
-            String method = httpServletRequest.getMethod();
-            if (!(Objects.equals(requestURI, "/api/omniflow/v1/user") && Objects.equals(method,"POST"))) {
-                String username = httpServletRequest.getHeader("username");
-                String token = httpServletRequest.getHeader("token");
-                if (!StrUtil.isAllNotBlank(username, token)) {
-                    returnJson((HttpServletResponse)servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
-                    return;
-                }
-                Object userInfoJsonStr;
-                try {
-                    userInfoJsonStr =  stringRedisTemplate.opsForHash().get("login_" + username, token);
-                    if (userInfoJsonStr == null) {
-                        throw new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL);
-                    }
-                } catch (Exception e) {
-                    returnJson((HttpServletResponse)servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
-                    return;
-                }
-                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-                UserContext.setUser(userInfoDTO);
-            }
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String requestURI = request.getRequestURI();
+
+        // 1. 是否在忽略列表
+        if (IGNORE_URI.contains(requestURI)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
         }
+
+        // 2. 获取token
+        String token = extractToken(request);
+        if (token == null) {
+            throw new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL);
+        }
+
+        // 3. 读取用户信息
+        Object userInfoJsonStr;
+        try {
+            userInfoJsonStr = stringRedisTemplate.opsForHash().get("login:" + request.getHeader("username"), token);
+            if (userInfoJsonStr == null) {
+                throw new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL);
+            }
+        } catch (Exception e) {
+            returnJson((HttpServletResponse)servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+            return;
+        }
+        UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+        UserContext.setUser(userInfoDTO);
+
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
@@ -73,4 +77,11 @@ public class UserTransmitFilter implements Filter {
             e.printStackTrace();
         }
     }
+
+    private String extractToken(HttpServletRequest request) {
+        // 从 Header 提取 Token
+        String header = request.getHeader("Authorization");
+        return (header != null && header.startsWith("Bearer ")) ? header.substring(7) : null;
+    }
+
 }
