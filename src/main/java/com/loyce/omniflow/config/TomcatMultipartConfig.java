65d59dc997757fdad973c505e6625ccf74679ee7
luo-jiu <z3201132097@gmail.com>
2026-04-05 14:51:25 +0800

    chore(upload): make multipart size limits configurable and raise defaults

--- src/main/java/com/loyce/omniflow/config/TomcatMultipartConfig.java
+++ src/main/java/com/loyce/omniflow/config/TomcatMultipartConfig.java
@@ -1,15 +1,20 @@
 package com.loyce.omniflow.config;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TomcatMultipartConfig {
 
+    @Value("${omniflow.upload.content-length-limit-bytes:1258291200}")
+    private String contentLengthLimitBytes;
+
     @PostConstruct
     public void init() {
         // 设置系统属性
         System.setProperty("org.apache.tomcat.util.http.fileupload.MultipartStream.HEADER_PART_SIZE_MAX", "8192");
-        System.setProperty("org.apache.tomcat.util.http.fileupload.FileUploadBase.CONTENT_LENGTH_LIMIT", "209715200");
+        // 需大于业务允许的 max-file-size，并给 multipart 边界留余量。
+        System.setProperty("org.apache.tomcat.util.http.fileupload.FileUploadBase.CONTENT_LENGTH_LIMIT", contentLengthLimitBytes);
     }
 }
